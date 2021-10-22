b8f4778b8c3d23f58e203ddacc67ad1f25702824
Yara Polana <yara.polana@gmail.com>
2021-10-22 14:58:15 -0700

    add login box to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { LoginBox } from './components/LoginBox'
 import styles from './styles/App.module.scss'
 
 export function App() {
 
   return (
     <main className={styles.contentWrapper}>
+      <LoginBox />
     </main>
   )
 }

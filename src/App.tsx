10dfd8ae8913f47caf9c7d643634484adad5359e
Yara Polana <yara.polana@gmail.com>
2021-10-22 15:53:05 -0700

    add message component and show correct component for when user is signed in

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
+import { SendMessageForm } from './components/SendMessageForm'
+import { useAuth } from './hooks/useAuth'
 import styles from './styles/App.module.scss'
 
 export function App() {
+  const { user } = useAuth()
 
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox />
+      { !!user ? <SendMessageForm /> : <LoginBox /> }
     </main>
   )
 }
